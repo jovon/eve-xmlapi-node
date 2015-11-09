ac9543432150e58820e4796c93debb6abf607e5b
Jovon <jvnpackard@gmail.com>
2015-11-09 22:59:40 -0100

    redis cache2

--- test.js
+++ test.js
@@ -2,11 +2,10 @@ var eve = require('./lib/EveClient')()
 
 eve.setCache('file')
 
-
 eve.serverStatus.fetch(function(err, data) {
-	if(err) console.log(err)
-	console.log(data)
-	return
+	if(err) console.log("test err", err)
+	if(data) console.log("test data", data.eveapi.result)
+	
 })
 
 
