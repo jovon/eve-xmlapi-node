d35d844fdedbb3119b0a3cbfc7a2b7bf8c0da590
Jovon Packard <jvnpackard@gmail.com>
2015-11-09 13:29:52 -0500

    reorg code

--- test.js
+++ test.js
@@ -3,9 +3,9 @@ var eve = require('./lib/EveClient')()
 eve.setCache('file')
 
 
-eve.characterID.fetch('Slithe Plundarr', function(err, data) {
-	if(err) console.log('CharacterID: ', err)
-	console.log('CharacterID: ',data)
+eve.serverStatus.fetch(function(err, data) {
+	if(err) console.log(err)
+	console.log(data)
 	return
 })
 
