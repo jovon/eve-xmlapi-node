9023fadc4c1140d1c38d86cc31fe51939ca29ee2
Jovon <jvnpackard@gmail.com>
2015-11-29 18:57:30 -0500

    migrate redis cache to typescript

---
+++ lib/cache/redis.d.ts
@@ -0,0 +1,11 @@
+import Cache = require('./cache');
+export = RedisCache;
+declare class RedisCache extends Cache {
+    private _port;
+    private _host;
+    private _client;
+    constructor(options: any);
+    clear(cb: Function): void;
+    write: (key: string, value: string, duration: number, cb: Function) => void;
+    read: (key: string, cb: Function) => void;
+}
