c25bf9586c12b4f74f8306253c90eece73d05b83
Jovon <jvnpackard@gmail.com>
2015-12-03 14:52:25 -0500

    remove typings folder

--- typings/tsd.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference path="bluebird/bluebird.d.ts" />
-/// <reference path="lodash/lodash.d.ts" />
-/// <reference path="node/node.d.ts" />
-/// <reference path="mocha/mocha.d.ts" />
-/// <reference path="qs/qs.d.ts" />
-/// <reference path="redis/redis.d.ts" />
-/// <reference path="xml2js/xml2js.d.ts" />
-
